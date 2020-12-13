0ceb7890f9a9e93ee79fc17efe2d4a6ca2f48d8b
Otto <otto.haapio@metropolia.fi>
2020-12-13 17:30:17 +0200

    Tehtäväpaketti 9

---
+++ exercise9_2.py
@@ -0,0 +1,9 @@
+from functions import *
+
+# pyydetään tunnit, minuutit ja sekunnit
+hours = int(input("Anna tunnit:\n"))
+minutes = int(input("Anna minuutit:\n"))
+seconds = int(input("Anna sekunnit:\n"))
+
+# laskee yhteen ja tulostaa
+count_seconds(hours, minutes, seconds)
